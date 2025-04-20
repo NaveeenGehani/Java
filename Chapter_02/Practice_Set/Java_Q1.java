1a1ebf6b0be0b56dceac14a30a651efb8885b9c8
Naveen <naveengehani999@gmail.com>
2025-04-20 19:54:09 +0500

    add Q1 of practice setof chapter 02

---
+++ Chapter_02/Practice_Set/Java_Q1.java
@@ -0,0 +1,8 @@
+package Practice_Set;
+
+public class Java_Q1 {
+    public static void main(String[] args) {
+        float a = 7/4.0f * 9/2.0f;
+        System.out.println(a); // prints 7.875
+    }
+}
