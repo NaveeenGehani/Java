55f475a96de62cc9af45f95786af0ee730290893
Naveen <naveengehani999@gmail.com>
2025-04-21 01:19:25 +0500

    add Q3 of practice set of chapter 02

---
+++ Chapter_02/Practice_Set/Java_Q3.java
@@ -0,0 +1,11 @@
+package Practice_Set;
+import java.util.Scanner;
+
+public class Java_Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int a = sc.nextInt();
+       System.out.println(a > 8);
+    }
+}
