81c1a94c66a005868a3ce2cee0489cfd326f557e
Naveen <naveengehani999@gmail.com>
2025-04-21 20:43:58 +0500

    Add Q1 of practice set of chapter 03

---
+++ Chapter_03/Practice_Set/Java_Q1.java
@@ -0,0 +1,8 @@
+package Chapter_03.Practice_Set;
+
+public class Java_Q1 {
+    public static void main(String[] args) {
+        String name = " NAVEEN GEHANI ";
+        System.out.println(name.toLowerCase());
+    }
+}
