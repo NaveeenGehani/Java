08f17a28006178d9ac99990ac499de398e86cb67
Naveen <naveengehani999@gmail.com>
2025-04-30 19:05:55 +0500

    Add task 02

---
+++ LABS/Lab_02/Squares_02.java
@@ -0,0 +1,13 @@
+package LABS.Lab_02;
+
+public class Squares_02 {
+    public static void main(String[] args) {
+        printSquares();
+    }
+    // Method for printing squares
+    public static void printSquares(){
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i*i);
+        }
+    }
+}
