939b414d8c64dfb0ec8a8991542faf98f722687d
Naveen <naveengehani999@gmail.com>
2025-05-01 23:38:26 +0500

    Add task 06

---
+++ LABS/Lab_02/weekDays_06.java
@@ -0,0 +1,12 @@
+package LABS.Lab_02;
+
+public class weekDays_06 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 4; i++){
+            System.out.format("Week %d\n",i);
+            for(int j = 1; j <= 7; j++){
+                System.out.format("\t Day %d\n", j);
+            }
+        }
+    }
+}
